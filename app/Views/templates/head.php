316f0fa61ffff1189620a9bf44c72caf300f46ab
aris <dovi.aristide@gmail.com>
2019-11-05 20:52:12 +0000

    Modification du fichier de config et head pour le sytle

--- app/Views/templates/head.php
+++ app/Views/templates/head.php
@@ -36,8 +36,8 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 
-    <base href="/">
+    
     <!-- Custom Stylesheet -->
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    "db_user" => "id10178787_root",
-    "db_pass" => "TJllome19930831",
+    "db_user" => "root",
+    "db_pass" => "root",
     "db_host" => "localhost",
-    "db_name" => "id10178787_rdv"
+    "db_name" => "rdv"
 );
